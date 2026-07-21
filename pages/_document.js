46ab71e279ded446973e8bb397aff73c20915742
widlyly413-create <widlyly413@gmail.com>
2026-07-21 17:37:16 +0800

    Add files via upload

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <title>重心重量计算系统</title>
-        <script async src="https://docs.opencv.org/4.8.0/opencv.js" onLoad="onOpenCvReady()" type="text/javascript"></script>
+        <script async src="/opencv.js" onLoad="onOpenCvReady()" type="text/javascript"></script>
       </Head>
       <body>
         <Main />
