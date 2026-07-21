f3387155d924e411634e46417c74131a9d765222
widlyly413-create <widlyly413@gmail.com>
2026-07-21 17:53:44 +0800

    Add files via upload

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
+  const cvInitScript = `
+    window.cvReady = false;
+    function onOpenCvReady() {
+      window.cvReady = true;
+      console.log('OpenCV.js loaded successfully');
+    }
+  `;
+
   return (
     <Html lang="zh-CN">
       <Head>
         <meta charSet="utf-8" />
         <title>重心重量计算系统</title>
-        <script type="text/javascript">
-          window.cvReady = false;
-          function onOpenCvReady() {
-            window.cvReady = true;
-            console.log('OpenCV.js loaded successfully');
-          }
-        </script>
+        <script type="text/javascript" dangerouslySetInnerHTML={{ __html: cvInitScript }} />
         <script async src="/opencv.js" onLoad="onOpenCvReady()" type="text/javascript"></script>
       </Head>
       <body>
